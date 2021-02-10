fec85ee49e1c57d4e31e746f63cc5c1a72b619b9
Marvin Countryman <me@maar.vin>
2021-02-10 15:41:56 -0500

    Add repository/update test and docs

---
+++ src/__tests__/repository/update.test.ts
@@ -0,0 +1,44 @@
+import { map, tap } from "rxjs/operators";
+import { update } from "../../repository/update";
+import { mockRepo } from "../helpers/repo_helper";
+
+describe("repository/update", () => {
+  /**
+   * Ensures {@link find} creates a valid `Get` request and processes response
+   * correctly.
+   */
+  test("Validate request/response", (done) => {
+    const entityId = "TEST_ENTITY_ID";
+    const entityName = "TEST_ENTITY_NAME";
+    const entityType = "TEST_ENTITY_TYPE";
+
+    const repo = mockRepo(entityType, (req$) =>
+      req$.pipe(
+        tap((req) => {
+          // Check method name
+          expect(req.method).toBe("Set");
+          // Check typeName (pulled from repository)
+          expect(req.params.typeName).toBe(entityType);
+          // Check entity.id (pulled from args)
+          expect(req.params.entity.id).toBe(entityId);
+          // Check entity.name (pulled from args)
+          expect(req.params.entity.name).toBe(entityName);
+        }),
+        map((req) => ({
+          ...req,
+          result: req.params.entity,
+        }))
+      )
+    );
+
+    update(repo, { id: entityId, name: entityName }).subscribe({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      next: (entity: any) => {
+        expect(entity.id).toBe(entityId);
+        expect(entity.name).toBe(entityName);
+      },
+      error: done,
+      complete: done,
+    });
+  });
+});
--- src/repository/update.ts
+++ src/repository/update.ts
@@ -1,10 +1,21 @@
 import { Observable } from "rxjs";
+import { IEntity } from "../models/entity";
 import { Repo, RepoEntity } from "./types";
 
+/**
+ * Update supplied entity in supplied repo.
+ *
+ * @param repo - The repository to update entity in.
+ * @param entity - The entity to update.
+ * @returns Observable of updated entity.
+ */
 export function update<
   TRepo extends Repo<TEntity>,
-  TEntity = RepoEntity<TRepo>
->(repo: TRepo, entity: Partial<TEntity>): Observable<string> {
+  TEntity extends IEntity = RepoEntity<TRepo>
+>(
+  repo: TRepo,
+  entity: Partial<TEntity> & Pick<TEntity, "id">
+): Observable<string> {
   return repo.client.call("Set", {
     entity,
     typeName: repo.typeName,
