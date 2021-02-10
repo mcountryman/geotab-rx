0a3e67ca6a11c2ce049ac0bf740c93de58673d4e
Marvin Countryman <me@maar.vin>
2021-02-10 15:12:24 -0500

    Add repository/count tests and documentation

--- src/repository/count.ts
+++ src/repository/count.ts
@@ -1,6 +1,13 @@
 import { Observable } from "rxjs";
 import { Repo } from "./types";
 
+/**
+ * Gets the count of entities in the supplied repository. Entities that are currently 
+ * inactive (the Entity's ActiveTo date is before the current time) are counted as well.
+ * 
+ * @param repo - The repository to get size of.
+ * @returns Number of entities in supplied repository.
+ */
 export function count<TRepo extends Repo<unknown>>(
   repo: Repo<TRepo>
 ): Observable<number> {
