67cbaf7b67e6b0e544bbb24831cb9fa7ce6e7b06
Marvin Countryman <me@maar.vin>
2021-02-10 15:37:14 -0500

    Add repository/remove test and documentation

---
+++ src/__tests__/repository/remove.test.ts
@@ -0,0 +1,36 @@
+import { map, tap } from "rxjs/operators";
+import { remove } from "../../repository/remove";
+import { mockRepo } from "../helpers/repo_helper";
+
+describe("repository/remove", () => {
+  /**
+   * Ensures {@link find} creates a valid `Get` request and processes response
+   * correctly.
+   */
+  test("Validate request/response", (done) => {
+    const entityId = "TEST_ENTITY_ID";
+    const entityType = "TEST_ENTITY_TYPE";
+
+    const repo = mockRepo(entityType, (req$) =>
+      req$.pipe(
+        tap((req) => {
+          // Check method name
+          expect(req.method).toBe("Remove");
+          // Check typeName (pulled from repository)
+          expect(req.params.typeName).toBe(entityType);
+          // Check search.id (pulled from args)
+          expect(req.params.entity.id).toBe(entityId);
+        }),
+        map((req) => ({
+          ...req,
+          result: {},
+        }))
+      )
+    );
+
+    remove(repo, { id: entityId }).subscribe({
+      error: done,
+      complete: done,
+    });
+  });
+});
--- src/repository/remove.ts
+++ src/repository/remove.ts
@@ -1,10 +1,25 @@
 import { Observable } from "rxjs";
+import { IEntity } from "../models/entity";
 import { Repo, RepoEntity } from "./types";
 
+/**
+ * Permanently removes an entity and it's associated data from supplied repository.  The
+ * entity must have an `id` field, remaining fields are optional.
+ * 
+ * @remarks
+ * 
+ * This can trigger cascading removal of all depanent objects associated with the entity.
+ * For example removing a {@link Device} would remove all {@link LogRecord}s associated 
+ * with it.
+ * 
+ * @param repo - The repo to remove entity from.
+ * @param entity - The entity to remove.
+ * @returns Squat.
+ */
 export function remove<
   TRepo extends Repo<TEntity>,
-  TEntity = RepoEntity<TRepo>
-  >(repo: TRepo, entity: Partial<TEntity>): Observable<void> {
+  TEntity extends IEntity = RepoEntity<TRepo>
+  >(repo: TRepo, entity: Partial<TEntity> & Pick<TEntity, "id">): Observable<void> {
   return repo.client.call("Remove", {
     entity,
     typeName: repo.typeName,
