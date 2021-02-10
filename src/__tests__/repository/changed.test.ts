7f57393db29ebb9f2a4126255e98af71a09c0167
Marvin Countryman <me@maar.vin>
2021-02-10 14:56:57 -0500

    Add repository/changed test
    * Add repository/changed test
    * Add repository/changed documentation

---
+++ src/__tests__/repository/changed.test.ts
@@ -0,0 +1,48 @@
+import { map, tap } from "rxjs/operators";
+import { changed } from "../../repository";
+import { mockRepo } from "../helpers/repo_helper";
+
+describe("repository/changed", () => {
+  /**
+   * Ensures {@link changed} creates a valid `GetFeed` request and processes response 
+   * correctly.
+   */
+  test("Validate request/response", (done) => {
+    const entityName = "TEST_ENTITY_NAME";
+    const entityType = "TEST_ENTITY_TYPE";
+
+    const feedData = [0, 1, 2, 3];
+    const feedVersion = "";
+
+    const repo = mockRepo(entityType, (req$) =>
+      req$.pipe(
+        tap((req) => {
+          // Check method name
+          expect(req.method).toBe("GetFeed");
+          // Check typeName (pulled from repository)
+          expect(req.params.typeName).toBe(entityType);
+          // Check search (pulled from create args)
+          expect(req.params.search.name).toBe(entityName);
+        }),
+        map((req) => ({
+          ...req,
+          result: {
+            data: feedData,
+            toVersion: feedVersion,
+          },
+        }))
+      )
+    );
+
+    changed(repo, { name: entityName }).subscribe({
+      next: (feed) => {
+        // Check feed data
+        expect(feed.data).toBe(feedData);
+        // Check feed version
+        expect(feed.toVersion).toBe(feedVersion);
+      },
+      error: done,
+      complete: done,
+    });
+  });
+});
--- src/repository/changed.ts
+++ src/repository/changed.ts
@@ -2,6 +2,31 @@ import { Observable } from "rxjs";
 import { IFeedResult } from "../models/feed_result";
 import { Repo, RepoEntity, RepoSearch, Search } from "./types";
 
+/**
+ * Fetches changed records since last request.  Feed result can contain only-new or 
+ * updated and new records depending on the entity type.
+ * 
+ * @see {@link https://geotab.github.io/sdk/software/guides/data-feed/#active-vs-calculated}
+ * for details on which feeds will return udpated results.
+ * 
+ * @remarks
+ * 
+ * It may be required to provide an entity search using from date to "back-fill" or "seed"
+ * data from a date in the past. Providing a from date guarantees that the feed will start
+ * at a version with all entities that have a date greater than or equal to the date 
+ * provided. However, it is possible that the feed will return entities before the 
+ * provided date. Searching using from date should be used independent of fromVersion and 
+ * only on the first request. 
+ * 
+ * @param repo - The repository to search for entities in.
+ * @param search - The search object for the type of data to return. 
+ * @param fromVersion - The Last retrieved version. All new data that has arrived after 
+ * this version will be returned in this call, up to a maximum of resultsLimit data 
+ * records. The FeedResult returned by the feed method will contain the highest version 
+ * for subsequent calls. When starting a new feed, if this value is not provided, the call
+ * will return only the toVersion (last version in the system). The start date can be 
+ * specified in the search argument.
+ */
 export function changed<
   TRepo extends Repo<TEntity, TSearch>,
   TEntity = RepoEntity<TRepo>,
