19baa6882994216f6d477008af06143dde266480
Marvin Countryman <me@maar.vin>
2021-02-10 15:28:57 -0500

    Add repository/find test/docs

---
+++ src/__tests__/repository/find.test.ts
@@ -0,0 +1,47 @@
+import { map, tap } from "rxjs/operators";
+import { find } from "../../repository";
+import { mockRepo } from "../helpers/repo_helper";
+
+describe("repository/find", () => {
+  /**
+   * Ensures {@link find} creates a valid `Get` request and processes response
+   * correctly.
+   */
+  test("Validate request/response", (done) => {
+    const entityId = "TEST_ENTITY_ID";
+    const entityType = "TEST_ENTITY_TYPE";
+    const entityName = "TEST_ENTITY_NAME";
+
+    const repo = mockRepo(entityType, (req$) =>
+      req$.pipe(
+        tap((req) => {
+          // Check method name
+          expect(req.method).toBe("Get");
+          // Check typeName (pulled from repository)
+          expect(req.params.typeName).toBe(entityType);
+          // Check search.id (pulled from args)
+          expect(req.params.search.id).toBe(entityId);
+        }),
+        map((req) => ({
+          ...req,
+          result: [
+            {
+              id: entityId,
+              name: entityName,
+            },
+          ],
+        }))
+      )
+    );
+
+    find(repo, { id: entityId }).subscribe({
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      next: (entity: any) => {
+        expect(entity.id).toBe(entityId);
+        expect(entity.name).toBe(entityName);
+      },
+      error: done,
+      complete: done,
+    });
+  });
+});
--- src/repository/find.ts
+++ src/repository/find.ts
@@ -2,11 +2,23 @@ import { from, Observable } from "rxjs";
 import { mergeMap } from "rxjs/operators";
 import { Repo, RepoEntity, RepoSearch, Search } from "./types";
 
+/**
+ * Find entities using supplied parameters.
+ * 
+ * @remarks
+ * 
+ * {@link Search.limit} determines how many records should be returned.  The default is 
+ * all records.
+ * 
+ * @param repo - The repo to find entities in.
+ * @param search - The search conditions.
+ * @returns Observable collection of entities found.
+ */
 export function find<
   TRepo extends Repo<TEntity, TSearch>,
   TEntity = RepoEntity<TRepo>,
   TSearch = RepoSearch<TRepo>
->(repo: TRepo, search: Search<TSearch>): Observable<TEntity> {
+>(repo: TRepo, search?: Search<TSearch>): Observable<TEntity> {
   const { limit: resultsLimit, ...input } = search ?? {};
   return repo.client
     .call<TEntity[]>("Get", {
