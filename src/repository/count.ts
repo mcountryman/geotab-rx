bd5173d52ad5073bfa51492039eec9187174dad0
Marvin Countryman <me@maar.vin>
2021-02-16 14:55:37 -0500

    Add doc example for create, count methods

--- src/repository/count.ts
+++ src/repository/count.ts
@@ -4,6 +4,20 @@ import { Repo } from "./types";
 /**
  * Gets the count of entities in the supplied repository. Entities that are currently
  * inactive (the Entity's ActiveTo date is before the current time) are counted as well.
+ * 
+ * @example
+ * Log number of users in database:
+ * ```
+ * import { Geotab } from "geotab-rx";
+ * import { count } from "geotab-rx/repository/count";
+ * 
+ * function logNumberOfUsers(geotab: Geotab) {
+ *   count(geotab.users)
+ *     .subscribe(count => 
+ *       console.log(`Number of users = '${count}'`)
+ *     );
+ * }
+ * ```
  *
  * @param repo - The repository to get size of.
  * @returns Number of entities in supplied repository.
--- src/repository/create.ts
+++ src/repository/create.ts
@@ -5,6 +5,27 @@ import { Repo, RepoEntity } from "./types";
  * Adds a new entity to the database. In most cases, the entity being added will need to
  * be fully constructed. In other words, all its properties need to be defined. These
  * requirements are defined in each entity definition.
+ * 
+ * @example
+ * Create a driver change record:
+ * ```
+ * import { Geotab } from "geotab-rx";
+ * import { create } from "geotab-rx/repository/create";
+ * import { IUser } from "geotab-rx/models/user";
+ * import { IDevice } from "geotab-rx/models/device";
+ * 
+ * function assignDriverToDevice(geotab: Geotab, driver: IUser, device: IDevice) {
+ *   create(geotab.driverChanges, {
+ *     type: "Driver",
+ *     driver,
+ *     device,
+ *     dateTime: new Date(),
+ *   })
+ *     .subscribe(changeId => 
+ *       console.log(`Created driver change with id = '${changeId}'`)
+ *     )
+ * }
+ * ```
  *
  * @param repo - The repository to add entity to.
  * @param entity - The entity to create.
@@ -13,7 +34,7 @@ import { Repo, RepoEntity } from "./types";
 export function create<
   TRepo extends Repo<TEntity>,
   TEntity = RepoEntity<TRepo>
->(repo: TRepo, entity: TEntity): Observable<string> {
+>(repo: TRepo, entity: Partial<TEntity>): Observable<string> {
   return repo.client.call("Add", {
     entity,
     typeName: repo.typeName,
