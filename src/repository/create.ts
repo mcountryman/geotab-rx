0d2990f96b40309f2c4b9b9b8b6d507598b9935d
Marvin Countryman <me@maar.vin>
2021-02-23 12:24:42 -0500

    Update create fn

--- src/repository/create.ts
+++ src/repository/create.ts
@@ -5,7 +5,7 @@ import { Repo, RepoEntity } from "./types";
  * Adds a new entity to the database. In most cases, the entity being added will need to
  * be fully constructed. In other words, all its properties need to be defined. These
  * requirements are defined in each entity definition.
- * 
+ *
  * @example
  * Create a driver change record:
  * ```
@@ -13,7 +13,7 @@ import { Repo, RepoEntity } from "./types";
  * import { create } from "geotab-rx/repository/create";
  * import { IUser } from "geotab-rx/models/user";
  * import { IDevice } from "geotab-rx/models/device";
- * 
+ *
  * function assignDriverToDevice(geotab: Geotab, driver: IUser, device: IDevice) {
  *   create(geotab.driverChanges, {
  *     type: "Driver",
@@ -21,7 +21,7 @@ import { Repo, RepoEntity } from "./types";
  *     device,
  *     dateTime: new Date(),
  *   })
- *     .subscribe(changeId => 
+ *     .subscribe(changeId =>
  *       console.log(`Created driver change with id = '${changeId}'`)
  *     )
  * }
@@ -31,10 +31,10 @@ import { Repo, RepoEntity } from "./types";
  * @param entity - The entity to create.
  * @returns - The newly created entity id.
  */
-export function create<
-  TRepo extends Repo<TEntity>,
-  TEntity = RepoEntity<TRepo>
->(repo: TRepo, entity: Partial<TEntity>): Observable<string> {
+export function create<TRepo extends Repo<TEntity>, TEntity = RepoEntity<TRepo>>(
+  repo: TRepo,
+  entity: Partial<TEntity>
+): Observable<string> {
   return repo.client.call("Add", {
     entity,
     typeName: repo.typeName,
